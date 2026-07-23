595d78150179c6c5c6c2f7ef0c96e2b88ba39960
Francesc Roy <francesc.roy@gmail.com>
2026-07-23 11:53:20 +0200

    Ja podent cridar a CiviCRM

--- hello_world/src/Controller/HelloController.php
+++ hello_world/src/Controller/HelloController.php
@@ -4,11 +4,38 @@
  * Contains \Drupal\hello_world\Controller\HelloController.
  */
 namespace Drupal\hello_world\Controller;
+
+use Civi\Api4\Contact;
+
 class HelloController {
   public function content() {
+
+    \Drupal::service('civicrm')->initialize();
+
+    $request = \Drupal::request();
+    $id = $request->query->get('id');
+
+    \Drupal::logger('my_module')->info('Something happened');
+    \Drupal::logger('my_module')->info($id);
+
+    $contacts = Contact::get(FALSE)
+    ->addSelect('id', 'display_name', 'email_primary.email')
+    ->addWhere('id', '=', intval($id))
+    ->execute();
+
+    $stringToShow = "";
+
+    // Nomes crear l'activitat si el dni es el correcte.
+    // Posar un alert de comfirmació
+    // I un altre controller que digui, la teva direcció és aquesta. Si no es correcte, contacta amb info@fespinal.com
+
+    foreach ($contacts as $contact) {
+      $stringToShow .= $contact['display_name'];
+    }
+
     return array(
       '#type' => 'markup',
-      '#markup' => t('Hello, World!'),
+      '#markup' => $stringToShow,
     );
   }
 }
